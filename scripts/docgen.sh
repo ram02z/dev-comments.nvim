043f3e54b9b7e6cf69c11317ed4e2622c75b0dfe
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-08-21 16:19:42 +0100

    v1

---
+++ scripts/docgen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Build parser
+cd ./tree-sitter-lua
+make dist
+cd -
+
+# Generate docs
+nvim --version
+exec nvim --headless --noplugin -u scripts/minimal_init.vim -c "luafile ./scripts/gendocs.lua" -c 'qa'
